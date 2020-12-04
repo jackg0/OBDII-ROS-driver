1443d7249abed69025f17fe4d12020cc84f67125
Jack Geissinger <jgeissinger@carnegierobotics.com>
2020-12-04 13:41:53 -0500

    driver: Add support for finding connections and udev.

--- README.md
+++ README.md
@@ -13,10 +13,10 @@ It will connect to OBDII USB interface (the first one for now): `/dev/ttyUSB0`
 
 # TODO
 
-- [ ] Port as parameter instead of default `/dev/ttyUSB0`
+- [x] Port as parameter instead of default `/dev/ttyUSB0`
 - [ ] ROS package (there is ROS node already)
 - [ ] Automaticaly publish all available data
-- [ ] Add udev rules
+- [x] Add udev rules
 
 # Tested
 
--- obdii-publisher.py
+++ obdii-publisher.py
@@ -1,22 +1,20 @@
 import obd
 import time
 import rospy
+import sys
 from std_msgs.msg import Float64
 
+rospy.init_node('obd2_ros_driver')
 
-rospy.init_node('OBDII-ROS-driver', anonymous=True)
-
-pubrpm = rospy.Publisher('rpm', Float64, queue_size=10)
-pubspeed = rospy.Publisher('speed', Float64, queue_size=10)
-pubthrottle = rospy.Publisher('throttle', Float64, queue_size=10)
-pubrelthrot = rospy.Publisher('rel_throttle', Float64, queue_size=10)
-pubaccd = rospy.Publisher('acc_pedal_d', Float64, queue_size=10)
-pubacce = rospy.Publisher('acc_pedal_e', Float64, queue_size=10)
-pubthrotact = rospy.Publisher('throttle_act', Float64, queue_size=10)
-pubengine = rospy.Publisher('engine_load', Float64, queue_size=10)
-pubpressure = rospy.Publisher('pressure', Float64, queue_size=10)
-pubfuel = rospy.Publisher('fuel', Float64, queue_size=10)
-
+pubrpm = rospy.Publisher('obd/rpm', Float64, queue_size=10)
+pubspeed = rospy.Publisher('obd/speed', Float64, queue_size=10)
+pubthrottle = rospy.Publisher('obd/throttle', Float64, queue_size=10)
+pubrelthrot = rospy.Publisher('obd/rel_throttle', Float64, queue_size=10)
+pubaccd = rospy.Publisher('obd/acc_pedal_d', Float64, queue_size=10)
+pubacce = rospy.Publisher('obd/acc_pedal_e', Float64, queue_size=10)
+pubthrotact = rospy.Publisher('obd/throttle_act', Float64, queue_size=10)
+pubengine = rospy.Publisher('obd/engine_load', Float64, queue_size=10)
+pubpressure = rospy.Publisher('obd/pressure', Float64, queue_size=10)
 
 def new_rpm(v):
     value = float(str(v).split(':')[-1].split(' ')[0])
@@ -49,28 +47,46 @@ def new_fuel(v):
     value = float(str(v).split(':')[-1].split(' ')[0])
     pubfuel.publish(value)
 
+def find_connection(ports):
+    for port in ports:
+        connection = obd.Async(port)
+        if connection.status() == obd.OBDStatus.CAR_CONNECTED:
+            return connection
+    return None
 
 if __name__ == '__main__':
     try:
-        port = "/dev/ttyUSB0"
+        obd.logger.setLevel(obd.logging.WARNING)
 
-        connection = obd.Async(portstr=port)
+        if len(sys.argv) < 2:
+            ports = obd.scan_serial()
+        else:
+            ports = [sys.argv[1]]
 
-        connection.watch(obd.commands.RPM, callback=new_rpm)
-        connection.watch(obd.commands.SPEED, callback=new_speed)
-        connection.watch(obd.commands.THROTTLE_POS, callback=new_throt)
-        connection.watch(obd.commands.RELATIVE_THROTTLE_POS, callback=new_relthrot)
-        connection.watch(obd.commands.ACCELERATOR_POS_D, callback=new_accd)
-        connection.watch(obd.commands.ACCELERATOR_POS_E, callback=new_acce)
-        connection.watch(obd.commands.THROTTLE_ACTUATOR, callback=new_throtact)
-        connection.watch(obd.commands.ENGINE_LOAD, callback=new_engine)
-        connection.watch(obd.commands.BAROMETRIC_PRESSURE, callback=new_pressure)
-        connection.watch(obd.commands.FUEL_STATUS, callback=new_fuel)
+        connection = find_connection(ports)
 
-        connection.start()
+        if connection is not None:
+            rospy.loginfo("OBD - Connection found for OBD {}...".format(connection.port_name()))
 
-        rospy.spin()
+            connection.watch(obd.commands.RPM, callback=new_rpm)
+            connection.watch(obd.commands.SPEED, callback=new_speed)
+            connection.watch(obd.commands.THROTTLE_POS, callback=new_throt)
+            connection.watch(obd.commands.RELATIVE_THROTTLE_POS, callback=new_relthrot)
+            connection.watch(obd.commands.ACCELERATOR_POS_D, callback=new_accd)
+            connection.watch(obd.commands.ACCELERATOR_POS_E, callback=new_acce)
+            connection.watch(obd.commands.THROTTLE_ACTUATOR, callback=new_throtact)
+            connection.watch(obd.commands.ENGINE_LOAD, callback=new_engine)
+            connection.watch(obd.commands.BAROMETRIC_PRESSURE, callback=new_pressure)
 
-        connection.stop()
+            rospy.loginfo("OBD - Starting OBD connection...")
+            connection.start()
+            rospy.loginfo("OBD - OBD connected...")
+
+            rospy.spin()
+
+            connection.stop()
+            rospy.loginfo("OBD - OBD disconnected...")
+
     except rospy.ROSInterruptException:
+        rospy.loginfo("OBD - ROS interrupted.")
         pass
